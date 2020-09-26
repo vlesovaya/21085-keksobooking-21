b214dc74b71cb3a48e1e333a04a119b3d6160f6c
valentinalesovaa <valentina-lesovaya@yandex.ru>
2020-09-26 16:54:27 +0300

    Добавила функцию создания DOM-элемента на основе JS-объекта

--- js/main.js
+++ js/main.js
@@ -7,25 +7,25 @@ function getRandomInteger(min, max) {
 function getMockAds() {
   let ads = [];
   for (let i = 1; i <= 8; i++) {
-    const checkinArr = ['12:00', '13:00', '14:00'];
-    const typeArr = ['palace', 'flat', 'house', 'bungalow'];
-    const apartFeatures = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
-    const apartPhotos = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
+    const checkinArr = [`12:00`, `13:00`, `14:00`];
+    const typeArr = [`palace`, `flat`, `house`, `bungalow`];
+    const apartFeatures = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
+    const apartPhotos = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
 
     const ad = {
       author: {
-        avatar: 'img/avatars/user0' + i + '.png',
+        avatar: `img/avatars/user0` + i + `.png`,
       },
       offer: {
-        title: 'name ' + i,
-        address: getRandomInteger(0, 600) + ', ' + getRandomInteger(0, 600),
+        title: `name ` + i,
+        address: getRandomInteger(0, 600) + `, ` + getRandomInteger(0, 600),
         price: getRandomInteger(0, 5000),
         type: typeArr[getRandomInteger(0, typeArr.length)],
         rooms: getRandomInteger(0, 4),
         guests: getRandomInteger(0, 4),
         checkin: checkinArr[getRandomInteger(0, checkinArr.length)],
         features: apartFeatures.slice(0, getRandomInteger(1, apartFeatures.length)),
-        description: 'description ' + i,
+        description: `description ` + i,
         photos: apartPhotos.slice(0, getRandomInteger(1, apartPhotos.length)),
       },
       location: {
@@ -40,5 +40,17 @@ function getMockAds() {
 
 const map = document.querySelector(`.map`);
 map.classList.remove(`map--faded`);
+
+function addPins() {
+  const pinsData = getMockAds();
+  const template = document.querySelector(`#pin`).content.querySelector(`button`);
+  for (let ad of pinsData) {
+    const pin = template.cloneNode(true);
+    pin.style.left = ad.location.x;
+    pin.style.top = ad.location.y;
+    map.appendChild(pin);
+  }
+}
+addPins(map);
 
 
