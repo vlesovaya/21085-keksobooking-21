0ab144097f7460238a8b4f5d5823ac38b7f14e5f
valentinalesovaa <valentina-lesovaya@yandex.ru>
2020-09-26 19:18:36 +0300

    Добавила функцию заполнения блока DOM-элементами на основе массива JS-объектов и внесла правки

--- js/main.js
+++ js/main.js
@@ -6,26 +6,28 @@ function getRandomInteger(min, max) {
 
 function getMockAds() {
   let ads = [];
-  for (let i = 1; i <= 8; i++) {
-    const checkinArr = [`12:00`, `13:00`, `14:00`];
-    const typeArr = [`palace`, `flat`, `house`, `bungalow`];
-    const apartFeatures = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
-    const apartPhotos = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
+  const apartCheckin = ["12:00", "13:00", "14:00"];
+  const apartCheckOut = ["12:00", "13:00", "14:00"];
+  const apartTypes = ["palace", "flat", "house", "bungalow"];
+  const apartFeatures = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+  const apartPhotos = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
 
+  for (let i = 1; i <= 8; i++) {
     const ad = {
       author: {
-        avatar: `img/avatars/user0` + i + `.png`,
+        avatar: "img/avatars/user0" + i + ".png",
       },
       offer: {
-        title: `name ` + i,
-        address: getRandomInteger(0, 600) + `, ` + getRandomInteger(0, 600),
+        title: "name" + i,
+        address: getRandomInteger(0, 600) + " , " + getRandomInteger(0, 600),
         price: getRandomInteger(0, 5000),
-        type: typeArr[getRandomInteger(0, typeArr.length)],
+        type: apartTypes[getRandomInteger(0, apartTypes.length)],
         rooms: getRandomInteger(0, 4),
         guests: getRandomInteger(0, 4),
-        checkin: checkinArr[getRandomInteger(0, checkinArr.length)],
+        checkin: apartCheckin [getRandomInteger(0, apartCheckin.length)],
+        checkout: apartCheckOut [getRandomInteger(0, apartCheckOut.length)],
         features: apartFeatures.slice(0, getRandomInteger(1, apartFeatures.length)),
-        description: `description ` + i,
+        description: "description" + i,
         photos: apartPhotos.slice(0, getRandomInteger(1, apartPhotos.length)),
       },
       location: {
@@ -38,19 +40,27 @@ function getMockAds() {
   return ads;
 }
 
-const map = document.querySelector(`.map`);
-map.classList.remove(`map--faded`);
+function removeFaded(mapElement) {
+  mapElement.classList.remove(`map--faded`);
+}
 
-function addPins() {
+function addPin(pinData, template, mapElement) {
+  const pin = template.cloneNode(true);
+  pin.style.left = pinData.location.x + pin.style.width / 2 + 'px';
+  pin.style.top = pinData.location.y - pin.style.height / 2 + 'px';
+  pin.querySelector(`img`).src = pinData.author.avatar;
+  pin.querySelector(`img`).alt = pinData.offer.title;
+  mapElement.appendChild(pin);
+}
+
+function addPins(mapElement) {
   const pinsData = getMockAds();
   const template = document.querySelector(`#pin`).content.querySelector(`button`);
   for (let ad of pinsData) {
-    const pin = template.cloneNode(true);
-    pin.style.left = ad.location.x;
-    pin.style.top = ad.location.y;
-    map.appendChild(pin);
+    addPin(ad, template, mapElement);
   }
 }
+
+const map = document.querySelector(`.map`);
+removeFaded(map);
 addPins(map);
-
-
