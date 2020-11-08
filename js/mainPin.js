dd338d5451844de6d1de115a0c5bfea282a472b8
valentinalesovaa <valentina-lesovaya@yandex.ru>
2020-11-08 01:00:59 +0100

    Добавила методы для обработки событий с мышки

--- js/mainPin.js
+++ js/mainPin.js
@@ -3,8 +3,10 @@
 (function () {
   let onActivationClickCallback;
   let onMoveCallback;
+  let moveMoveEnabled = false;
+  let isActive = false;
 
-  function processMove() {
+  function didMove() {
     const mapPin = window.elements.mapPin;
     const overlay = window.elements.mapOverlay();
     const PIN_TIP_HEIGHT = 15;
@@ -13,18 +15,64 @@
     const mapPinRect = mapPin.getBoundingClientRect();
     const leftPinPosition = Math.round((mapPinRect.width / 2) + mapPinRect.left - overlayRect.left);
     const topPinPosition = Math.round(mapPinRect.top + mapPinRect.height - overlayRect.top);
-    onMoveCallback(leftPinPosition, topPinPosition, PIN_TIP_HEIGHT);
+
+    if (onMoveCallback !== null) {
+      onMoveCallback(leftPinPosition, topPinPosition, PIN_TIP_HEIGHT);
+    }
+  };
+
+  function processMove(shiftX, shiftY) {
+    const mapPin = window.elements.mapPin;
+    let left = mapPin.style.left - `px`;
+    let top = mapPin.style.top - `px`;
+    mapPin.style.left = (left + shiftX) + `px`;
+    mapPin.style.top = (top + shiftY) + `px`;
   };
 
   function setupClick() {
     const mapPin = window.elements.mapPin;
+
     mapPin.addEventListener(`mousedown`, function (evt) {
-      if (evt.button === 0) {
+      if (evt.button !== 0) {
+        return;
+      }
+
+      if (isActive) {
+        moveMoveEnabled = true;
+      }
+
+      if (onActivationClickCallback !== null && !isActive) {
+        isActive = true;
         onActivationClickCallback();
       }
+
+      const startX = evt.clientX;
+      const startY = evt.clientY;
+
+      mapPin.addEventListener(`mousemove`, function (moveEvt) {
+        if (evt.button !== 0 || moveMoveEnabled === false) {
+          return;
+        }
+        const shiftX = startX - moveEvt.clientX;
+        const shiftY = startY - moveEvt.clientY;
+
+        processMove(shiftX, shiftY);
+      });
+
+      mapPin.addEventListener(`mouseup`, function (evt) {
+        if (evt.button === 0) {
+          didMove();
+        }
+        moveMoveEnabled = false;
+      });
     });
+
     mapPin.addEventListener(`keydown`, function (evt) {
-      if (evt.key === `Enter`) {
+      if (evt.key !== `Enter`) {
+        return;
+      }
+      if (onActivationClickCallback !== null || isActive) {
+        isActive = true;
         onActivationClickCallback();
       }
     });
@@ -40,12 +88,12 @@
 
   const setup = function () {
     setupClick();
-    processMove();
+    didMove();
   };
 
   window.mainPin = {
     setup,
     onActivationClick,
-    onMove
+    onMove,
   };
 })();
